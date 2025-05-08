09f08d7c8ea3a15d23971291f5bb7b8f9b43a6f9
Yuhei-shika <shikanono.1@gmail.com>
2025-05-08 12:47:55 +0900

    Error handle2

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 15 },
-    { name: "Jiro"}
-     
-  ];
-  return (
-    <div>
-      
-      {profiles.map((profile, index) => {
-        return (
-          <User name={profile.name} age={profile.age} key={index} />
-        );
-      })}
-    </div>
-  )
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
-
-const User = (props) => {
-  return (
-    <div>Hi, I am {props.name}, and {props.age} years old!</div>
-  );
-}
-
-User.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number
-};
 export default App;
