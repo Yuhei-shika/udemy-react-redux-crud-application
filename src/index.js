1df6970f10e91d95951f11ef773e1346cb2c3898
Yuhei-shika <shikanono.1@gmail.com>
2025-05-15 17:08:24 +0900

    再初期化：クリーンな状態からpush

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import EventsIndex from './components/events_index';
+import reducer from './reducers'
+import { thunk } from 'redux-thunk'
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <EventsIndex />
+    </Provider>,
+    document.getElementById('root')
+);
