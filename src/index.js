7ff29ab4ed1b4bf28cab7767883e222f8c2ed21f
Yuhei-shika <shikanono.1@gmail.com>
2025-05-15 17:55:50 +0900

    implement-events-new

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import reducer from './reducers'
 import { thunk } from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
-        <EventsIndex />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/events/new" component={EventsNew} />
+                <Route exact path="/" component={EventsIndex} />
+            </Switch>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
