e5138382c10070396ac1c3e77fa3f09b7d4bbbb3
Yuhei-shika <shikanono.1@gmail.com>
2025-05-18 18:55:37 +0900

    finish ch41

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -38,14 +38,14 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-   // await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
   render() {
     console.log('initialValues:', this.props.initialValues); // ← ここ！
   
-    const { handleSubmit, pristine, submitting } = this.props;
+    const { handleSubmit, pristine, submitting, invalid } = this.props;
   
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -55,7 +55,7 @@ class EventsShow extends Component {
         </div>
   
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting}/>
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid}/>
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>   
@@ -75,7 +75,7 @@ const mapStateToProps = (state, ownProps) => {
   const event = state.events[ownProps.match.params.id]
   return { initialValues: event, event }
 }
-const mapDispatchToProps = ({deleteEvent, getEvent})
+const mapDispatchToProps = ({deleteEvent, getEvent, putEvent})
 
 export default connect(mapStateToProps, mapDispatchToProps)(
   reduxForm({form: 'eventShowForm', enableReinitialize: true, validate})(EventsShow)
